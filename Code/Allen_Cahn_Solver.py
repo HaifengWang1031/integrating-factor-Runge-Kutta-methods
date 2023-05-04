d6229fb0de5ac9dd94a4dbb43cc6b74b143128fb
Haifeng Wang <hf_wang1031@163.com>
2023-05-04 12:18:28 +0800

    neo

--- Code/Allen_Cahn_Solver.py
+++ Code/Allen_Cahn_Solver.py
@@ -72,6 +72,12 @@ class Allen_Cahn_Solver_1D():
             self.expM13 = self.Q@np.diag(np.exp(1/3*self.tau*self.D))@self.Q_inv
             self.expM23 = self.Q@np.diag(np.exp(2/3*self.tau*self.D))@self.Q_inv
 
+        elif step_method == "Shu_Osher3":
+            self.step = self.Shu_Osher3
+            self.expM = self.Q@np.diag(np.exp(self.tau*self.D))@self.Q_inv
+            self.expM12 = self.Q@np.diag(np.exp(1/2*self.tau*self.D))@self.Q_inv
+            self.expMm12 = self.Q@np.diag(np.exp(-1/2*self.tau*self.D))@self.Q_inv
+
         elif step_method == "IFRK4":
             self.step = self.IFRK4
             self.expM = self.Q@np.diag(np.exp(self.tau*self.D))@self.Q_inv
@@ -110,6 +116,12 @@ class Allen_Cahn_Solver_1D():
             self.phiM30 = np.linalg.solve(self.phi3,self.phi0)
             self.phiM31 = np.linalg.solve(self.phi3,self.phi1)
             self.phiM32 = np.linalg.solve(self.phi3,self.phi2)
+        
+        elif step_method == "EFRK4":
+            pass
+
+        elif step_method == "EFRK54":
+            pass
 
     # Linear operators 
     def laplace_periodic(self):
@@ -164,6 +176,12 @@ class Allen_Cahn_Solver_1D():
         return 59/128*self.expM@Un +\
                15/128*self.expM@(Un + 4/3*self.tau*self.f(Un)) +\
                54/128*self.expM13@(U2 + 4/3*self.tau*self.f(U2))
+            
+    def Shu_Osher3(self,Un):
+        U1 = self.expM@(Un + self.tau*self.f(Un))
+        U2 = 3/4*self.expM12@Un + 1/4*self.expMm12@(U1 + self.tau*self.f(U1))
+        return 1/3*self.expM@Un +\
+               2/3*self.expM12@(U2 + self.tau*self.f(U2))
 
     def IFRK4(self,Un):
         U1 = self.expM12@(Un + self.tau/2*self.f(Un))
@@ -189,11 +207,17 @@ class Allen_Cahn_Solver_1D():
                15/128*self.phiM30@(Un + 4/3*self.tau*self.f(Un)) +\
                54/128*self.phiM32@(U2 + 4/3*self.tau*self.f(U2))
     
+    def EFRK4(self,Un):
+        pass
+
+    def EFRK54(self,Un):
+        pass
+    
     def energy(self,Un):
         energy = np.nan
         if self.bc == "periodic":
-        #    energy = self.h*np.sum(self.eps**2/2*(self.G@Un)**2 + self.F(Un))
-           energy = self.h*(-1/2*(Un[np.newaxis,:])@(self.L@(Un[:,np.newaxis])) + np.sum(self.F(Un)))
+           energy = self.h*np.sum(self.eps**2/2*(self.G@Un)**2 + self.F(Un))
+        #    energy = self.h*(-1/2*Un@self.L@Un) + np.sum(self.F(Un))
         elif self.bc == "neumann":
            energy = self.h*np.sum((self.eps**2/2*(self.G@Un)**2 + self.F(Un))[:-1])
         return energy
@@ -216,6 +240,214 @@ class Allen_Cahn_Solver_1D():
             pbar.update(self.tau)
         pbar.close()
 
+class Allen_Cahn_fSolver_1D():
+    def __init__(
+        self,
+        eps:float,
+        t_period:float,
+        s_domain:Tuple[float,float],
+        time_step:float,
+        discrete_num: int,
+        initial_condition: tnp.NDArray,
+        boundary_condition: str,
+        potential: str,
+        step_method: str) -> None:
+
+        self.eps = eps
+
+        self.T = t_period
+        self.tau = time_step
+        self.tn = np.array([0])
+
+        self.xa,self.xb = s_domain
+        self.N = discrete_num
+        self.h = (self.xb-self.xa)/self.N
+        self.xh = np.linspace(self.xa,self.xb,self.N+1)
+
+        u0 = initial_condition
+
+        self.bc = boundary_condition
+        if self.bc == "periodic":
+            self.L = self.eps**2*self.laplace_periodic()
+            self.D = np.array([-4*self.eps**2*np.sin(k*np.pi/self.N)**2/self.h**2 for k in range(self.N)])
+
+            self.U = u0[:-1][np.newaxis,:]
+            self.fU = np.fft.fft(u0[:-1])[np.newaxis,:]
+        elif self.bc == "neumann":
+            raise
+
+        if potential == "flory_huggins":
+            self.F,self.f = self.fh_potential()
+        elif potential == "ginzburg_landau":
+            self.F,self.f = self.gl_potential()
+        
+        self.Energys = np.array(self.energy(self.U[0]))
+
+        if step_method == "IFRK1":
+            self.step = self.IFRK1
+            self.expM = np.exp(self.tau*self.D)
+
+        elif step_method == "IFRK2":
+            self.step = self.IFRK2
+            self.expM = np.exp(self.tau*self.D)
+
+        # elif step_method == "IFRK3":
+        #     self.step = self.IFRK3
+        #     self.expM = np.exp(self.tau*self.D)
+        #     self.expM13 = np.exp(1/3*self.tau*self.D)
+        #     self.expM23 = np.exp(2/3*self.tau*self.D)
+
+        # elif step_method == "Shu_Osher3":
+        #     self.step = self.Shu_Osher3
+        #     self.expM = np.exp(self.tau*self.D)
+        #     self.expM12 = np.exp(1/2*self.tau*self.D)
+        #     self.expMm12 = np.exp(-1/2*self.tau*self.D)
+
+        # elif step_method == "IFRK4":
+        #     self.step = self.IFRK4
+        #     self.expM = np.exp(self.tau*self.D)
+        #     self.expM12 = np.exp(1/2*self.tau*self.D)
+        
+        # elif step_method == "EFRK1":
+        #     self.step = self.EFRK1
+        #     self.phi0 = np.ones(self.N)
+
+        #     self.phi1 = self.phi0@(np.ones(self.N) - self.tau*self.D)
+        #     self.phiM10 = self.phi0/self.phi1
+        
+        # elif step_method == "EFRK2":
+        #     self.step = self.EFRK2
+        #     self.phi0 = np.ones(self.N)
+
+        #     self.phi1 = self.phi0@(np.ones(self.N) - self.tau*self.D)
+        #     self.phiM10 = self.phi0/self.phi1
+
+        #     self.phi2 = 1/2*self.phi0 + 1/2*self.phi1@(np.ones(self.N) - self.tau*self.D) 
+        #     self.phiM20 = self.phi0/self.phi2
+        #     self.phiM21 = self.phi1/self.phi2
+        
+        # elif step_method == "EFRK3":
+        #     self.step = self.EFRK3
+        #     self.phi0 = np.ones(self.N)
+
+        #     self.phi1 = self.phi0@(np.ones(self.N) - 2/3*self.tau*self.D)
+        #     self.phiM10 = self.phi0/self.phi1
+
+        #     self.phi2 = 2/3*self.phi0 + 1/3*self.phi1@(np.ones(self.N) - 4/3*self.tau*self.D)
+        #     self.phiM20 = self.phi0/self.phi2
+        #     self.phiM21 = self.phi1/self.phi2
+
+        #     self.phi3 = 59/128*self.phi0 + 15/128*self.phi0@(np.ones(self.N) - 4/3*self.tau*self.D) + 27/64*self.phi2@(np.ones(self.N) - 4/3*self.tau*self.D)
+        #     self.phiM30 = self.phi0/self.phi3
+        #     self.phiM31 = self.phi1/self.phi3
+        #     self.phiM32 = self.phi2/self.phi3
+        
+        # elif step_method == "EFRK4":
+        #     pass
+
+        # elif step_method == "EFRK54":
+        #     pass
+
+    # Linear operators 
+    def laplace_periodic(self):
+        L = np.diag(-2*np.ones(self.N)) +\
+            np.diag(np.ones(self.N-1),1) +\
+            np.diag(np.ones(self.N-1),-1)
+        L[0,-1] = L[-1,0] = 1; L /= self.h**2
+        return L
+
+    # Potential and correspond nonlinear term
+    # f(u) = -F'(u)
+    def gl_potential(self):
+        F = lambda u: 1/4*(1-u**2)**2
+        f = lambda u: (u-u**3)
+        return F,f
+
+    def fh_potential(self,theta = 0.8,theta_c = 1.6):
+        F =lambda u: theta/2*((1+u)*np.log(1+u) + (1-u)*np.log(1-u)) - theta_c/2*u**2
+        f =lambda u: theta/2*np.log((1-u)/(1+u)) + theta_c*u
+        return F,f
+
+    # Time step method
+        # IFRK
+    def IFRK1(self,fUn,Un):
+        return self.expM*(fUn + self.tau*np.fft.fft(self.f(Un)))
+
+    def IFRK2(self,fUn,Un):
+        fU1 = self.expM*(fUn + self.tau*np.fft.fft(self.f(Un)))
+        return 1/2*self.expM*fUn +\
+               1/2*(fU1 + self.tau*np.fft.fft(self.f(np.real(np.fft.ifft(fU1)))))
+
+    # def IFRK3(self,fUn,Un):
+    #     U1 = self.expM23*(Un + 2*self.tau/3*self.f(Un))
+    #     U2 = 2/3*self.expM23*Un + 1/3*(U1 + 4*self.tau/3*self.f(U1))
+    #     return 59/128*self.expM*Un +\
+    #            15/128*self.expM*(Un + 4/3*self.tau*self.f(Un)) +\
+    #            54/128*self.expM13*(U2 + 4/3*self.tau*self.f(U2))
+            
+    # def Shu_Osher3(self,Un):
+    #     U1 = self.expM*(Un + self.tau*self.f(Un))
+    #     U2 = 3/4*self.expM12*Un + 1/4*self.expMm12@(U1 + self.tau*self.f(U1))
+    #     return 1/3*self.expM*Un +\
+    #            2/3*self.expM12*(U2 + self.tau*self.f(U2))
+
+    # def IFRK4(self,Un):
+    #     U1 = self.expM12*(Un + self.tau/2*self.f(Un))
+    #     U2 = self.expM12*Un + self.tau/2*self.f(U1)
+    #     U3 = self.expM*Un + self.tau*self.expM12*self.f(U2)
+
+    #     return 1/3*self.expM12*(U1 + self.tau/2*self.f(U1)) +\
+    #            1/3*self.expM12*U2 +\
+    #            1/3*(U3 + self.tau/2*self.f(U3))
+
+    #     # EFRK
+    # def EFRK1(self,Un):
+    #     return self.phiM10*(Un + self.tau*self.f(Un))
+    
+    # def EFRK2(self,Un):
+    #     U1 = self.phiM10*(Un + self.tau*self.f(Un))
+    #     return 1/2*self.phiM20*Un + 1/2*self.phiM21*(U1 + self.tau*self.f(U1))
+
+    # def EFRK3(self,Un):
+    #     U1 = self.phiM10*(Un + 2*self.tau/3*self.f(Un))
+    #     U2 = 2/3*self.phiM20*Un + 1/3*self.phiM21*(U1 + 4*self.tau/3*self.f(U1))
+    #     return 59/128*self.phiM30*Un +\
+    #            15/128*self.phiM30*(Un + 4/3*self.tau*self.f(Un)) +\
+    #            54/128*self.phiM32*(U2 + 4/3*self.tau*self.f(U2))
+    
+    # def EFRK4(self,Un):
+    #     pass
+
+    # def EFRK54(self,Un):
+    #     pass
+    
+    def energy(self,Un):
+        energy = np.nan
+        if self.bc == "periodic":
+           energy = self.h*(-1/2*Un@self.L@Un + self.F(Un)@np.ones(self.N))
+        elif self.bc == "neumann":
+            pass
+        return energy
+
+    def solve(self):
+        pbar = tqdm.tqdm(total=self.T+self.tau,
+                         bar_format="{desc}: {percentage:.2f}% |{bar}| {n:.2f}/{total:.2f}[{elapsed}<{remaining}] {postfix}",
+                         mininterval=0.1)
+
+        while self.tn[-1] < self.T:
+            fu = self.step(self.fU[-1],self.U[-1])
+
+            self.tn = np.append(self.tn,np.round(self.tn[-1]+self.tau,12))
+            
+            self.fU = np.concatenate([self.fU,fu[np.newaxis,:]],axis=0) 
+            self.U = np.concatenate([self.U,np.real(np.fft.ifft(fu))[np.newaxis,:]],axis=0) 
+            self.Energys = np.append(self.Energys,self.energy(self.U[-1]))
+            
+            pbar.set_postfix({"time_step":self.tau,
+                              "energy":self.Energys[-1],
+                              "maximum_val":np.max(self.U[-1])})
+            pbar.update(self.tau)
+        pbar.close()
 
 class Allen_Cahn_Solver_2D():
     def __init__(
@@ -653,15 +885,30 @@ class Allen_Cahn_Solver_3D():
 if __name__ == "__main__":
     eps = 0.01
     t_period = 1
-    s_domain = (0,0,1,1)
-    N = 8
-    discrete_num = N,N
+    s_domain = (0,1)
+    N = 16
+    discrete_num = N
     xn = np.linspace(s_domain[0],s_domain[1],N+1)
 
-    initial_condition = np.random.rand(N+1,N+1)
+    initial_condition = np.random.rand(N+1)
     boundary_condition = "periodic"
     potential = "flory_huggins" 
-    solver = Allen_Cahn_Solver_2D(eps,t_period,s_domain,2**-6,discrete_num,initial_condition,boundary_condition,potential,"IFRK3")
+    solver = Allen_Cahn_Solver_1D(eps,t_period,s_domain,2**-6,discrete_num,initial_condition,boundary_condition,potential,"IFRK3")
+
+    print(solver.L@initial_condition[:-1])
+    print(np.real(np.fft.ifft(np.fft.fft(initial_condition[:-1])*[-4*eps**2*np.sin(k*np.pi/N)**2/solver.h**2 for k in range(N)])))
+
+    # eps = 0.01
+    # t_period = 1
+    # s_domain = (0,0,1,1)
+    # N = 8
+    # discrete_num = N,N
+    # xn = np.linspace(s_domain[0],s_domain[1],N+1)
+
+    # initial_condition = np.random.rand(N+1,N+1)
+    # boundary_condition = "periodic"
+    # potential = "flory_huggins" 
+    # solver = Allen_Cahn_Solver_2D(eps,t_period,s_domain,2**-6,discrete_num,initial_condition,boundary_condition,potential,"IFRK3")
     # solver.solve()
 
     # print(solver.L@solver.U[-1] - np.real(np.fft.ifft(np.fft.fft(solver.L[:,0])*np.fft.fft(solver.U[-1]))))
